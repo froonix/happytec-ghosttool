54f35283b6f989e2d11a7160212ca087688b2ece
Christian Schrötter <cs@fnx.li>
2016-12-29 13:26:41 +0100

    Improve HTGT.sh

--- HTGT.sh
+++ HTGT.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 cd "$(dirname $0)" && \
 java -jar "HTGT.jar" && \
-exit $?
+exit $? || status=$?
+
+echo; echo "Das Programm wurde mit einem Fehler beendet: $status"
+read -r -p "Zum Beenden die RETURN-Taste drücken..." dummy; echo
