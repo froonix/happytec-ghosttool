froonix/happytec-ghosttool

commit cd7b5956575586329523dff8651afa788d378e7e

Improve FFM for 3TC (closes #68, closes #69)

src/HTGT.java
@@ -875,7 +875,7 @@ public class HTGT
 					// Diese API-Anfrage ist hier noch nicht notwendig.
 					// Dadurch wird aber schon hier geprüft, ob der Token
 					// gültig ist und ob aktive Strecken verfügbar sind.
-					int[][][] results = api.getAllResults();
+					int[][][][] results = api.getAllResults();
 
 					while(true)
 					{
@@ -970,6 +970,11 @@ public class HTGT
 											lastUploadedMode = m;
 											lastUploadedTrack = t;
 											lastUploadedWeather = w;
+
+											if(newProfileGhosts[m][t][w].hasTicket())
+											{
+												lastUploadedWeather = gmHelper.WEATHER_TICKET;
+											}
 										}
 									}
 								}
@@ -998,6 +1003,11 @@ public class HTGT
 												lastUploadedTrack = t;
 												lastUploadedWeather = w;
 												lastFromDefault = true;
+
+												if(newDefaultGhosts[m][t][w].hasTicket())
+												{
+													lastUploadedWeather = gmHelper.WEATHER_TICKET;
+												}
 											}
 										}
 									}
@@ -1023,8 +1033,14 @@ public class HTGT
 									int w = (int) item.get(2);
 									int t = (int) item.get(1);
 									int m = (int) item.get(0);
+									int o = eSportsAPI.FO_NONE;
 
-									if(results[m][t][w] == -1|| ghost.hasTicket() || (!gmHelper.isReverseGameMode(m) && ghost.getTime() < results[m][t][w]) || (gmHelper.isReverseGameMode(m) && ghost.getTime() > results[m][t][w]))
+									if(ghost.hasTicket())
+									{
+										o = eSportsAPI.FO_TICKET;
+									}
+
+									if(results[o][m][t][w] == -1|| (!gmHelper.isReverseGameMode(m) && ghost.getTime() < results[o][m][t][w]) || (gmHelper.isReverseGameMode(m) && ghost.getTime() > results[o][m][t][w]))
 									{
 										dbg(String.format("Uploading ghost: %s", ghost.getDebugDetails()));
 										ghostUpload(ghost, true);
@@ -1032,7 +1048,7 @@ public class HTGT
 									}
 									else
 									{
-										dbg(String.format("Ghost upload not possible, because old result (%d) is better or equal: %s", results[m][t][w], ghost.getDebugDetails()));
+										dbg(String.format("Ghost upload not possible, because old result (%d) is better or equal: %s", results[o][m][t][w], ghost.getDebugDetails()));
 
 										if(force)
 										{
@@ -1043,16 +1059,17 @@ public class HTGT
 								}
 							}
 
-							if(realUpload && lastUploadedMode > -1 && lastUploadedTrack > -1 && lastUploadedWeather > -1)
+							if(realUpload && lastUploadedMode > -1 && lastUploadedTrack > -1 && lastUploadedWeather != -1)
 							{
-								if(/*lastFromDefault &&*/ newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather] != null)
+								if(/*lastFromDefault &&*/ lastUploadedWeather > 0 && newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather] != null)
 								{
 									currentGhost = String.format("%nDer aktuell genutzte Geist ist von %s mit dem Ergebnis %s.%n", newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather].getNickname(), newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather].getResult());
 								}
 
 								if(cfg(CFG_NDG) == null)
 								{
-									int action = threesomeDialog(FF_TITLE, String.format("Willst du für %s (%s/%s) einen neuen Geist herunterladen?%n%s%nBitte beachte, dass die Datei danach automatisch gespeichert wird!", gmHelper.getTrack(tracks[lastUploadedTrack]), gmHelper.getGameModeName(modes[lastUploadedMode]), gmHelper.getWeatherName(weathers[lastUploadedWeather]), currentGhost), false);
+									int realWeather = (lastUploadedWeather == gmHelper.WEATHER_TICKET) ? gmHelper.WEATHER_TICKET : weathers[lastUploadedWeather];
+									int action = threesomeDialog(FF_TITLE, String.format("Willst du für %s (%s/%s) einen neuen Geist herunterladen?%n%s%nBitte beachte, dass die Datei danach automatisch gespeichert wird!", gmHelper.getTrack(tracks[lastUploadedTrack]), gmHelper.getGameModeName(modes[lastUploadedMode]), gmHelper.getWeatherName(realWeather), currentGhost), false);
 
 									if(action == BUTTON_NEVER)
 									{
@@ -1060,7 +1077,7 @@ public class HTGT
 									}
 									else if(action == BUTTON_YES)
 									{
-										if(ghostSelect(modes[lastUploadedMode], tracks[lastUploadedTrack], weathers[lastUploadedWeather], true))
+										if(ghostSelect(modes[lastUploadedMode], tracks[lastUploadedTrack], realWeather, true, ((realWeather == gmHelper.WEATHER_TICKET) ? true : false)))
 										{
 											if(OfflineProfiles.changed() && !saveFile(true))
 											{

src/eSportsAPI.java
@@ -70,10 +70,11 @@ public class eSportsAPI
 	private final static int RESULT_TYPE_PREV = 1;
 	private int[] lastTypeIndex = new int[2];
 
-//	private final static int FO_REV    = -1;
-	private final static int FO_NONE   =  0;
-	private final static int FO_TICKET =  1;
-//	private final static int FO_ALL    =  1;
+	public final static int FOS       =  2;
+//	public final static int FO_REV    = -1;
+	public final static int FO_NONE   =  0;
+	public final static int FO_TICKET =  1;
+//	public final static int FO_ALL    =  1;
 
 	public eSportsAPI(String token)
 	{
@@ -349,12 +350,12 @@ public class eSportsAPI
 		}
 	}
 
-	public int[][][] getAllResults() throws eSportsAPIException
+	public int[][][][] getAllResults() throws eSportsAPIException
 	{
 		return this.getSelectiveResults(null);
 	}
 
-	public int[][][] getSelectiveResults(int[][] filter) throws eSportsAPIException
+	public int[][][][] getSelectiveResults(int[][] filter) throws eSportsAPIException
 	{
 		int[] modes = gmHelper.getGameModeIDs();
 		String[] tracks = gmHelper.getTracks(true);
@@ -362,15 +363,18 @@ public class eSportsAPI
 
 		// Es ist intern wesentlich einfacher mit numerischen Schlüsseln zu arbeiten.
 		// Eine Map würde nur unnötigen Overhead erzeugen, der nicht notwendig ist.
-		int results[][][] = new int[modes.length][tracks.length][weathers.length];
+		int results[][][][] = new int[this.FOS][modes.length][tracks.length][weathers.length];
 
-		for(int m = 0; m < modes.length; m++)
+		for(int o = 0; o < this.FOS; o++)
 		{
-			for(int t = 0; t < tracks.length; t++)
+			for(int m = 0; m < modes.length; m++)
 			{
-				for(int w = 0; w < weathers.length; w++)
+				for(int t = 0; t < tracks.length; t++)
 				{
-					results[m][t][w] = -1;
+					for(int w = 0; w < weathers.length; w++)
+					{
+						results[o][m][t][w] = -1;
+					}
 				}
 			}
 		}
@@ -378,7 +382,7 @@ public class eSportsAPI
 		try
 		{
 			Map<String,Object> args = new HashMap<String,Object>();
-			args.put("includeTicket", "false"); // DO NOT USE TRUE!
+			args.put("includeTicket", "true"); // DO NOT USE FALSE!
 
 			if(filter != null)
 			{
@@ -415,6 +419,7 @@ public class eSportsAPI
 					String weather = OfflineResult.getAttribute("Weather").toLowerCase();
 					String gamemode = OfflineResult.getAttribute("GameMode").toLowerCase();
 
+					int o = this.FO_NONE;
 					int m = -1;
 					int t = -1;
 					int w = -1;
@@ -446,12 +451,17 @@ public class eSportsAPI
 						}
 					}
 
-					if(m == -1 || t == -1 || w == -1 || results[m][t][w] != -1)
+					if(OfflineResult.getAttribute("Ticket").toLowerCase().equals("true"))
+					{
+						o = this.FO_TICKET;
+					}
+
+					if(m == -1 || t == -1 || w == -1 || results[o][m][t][w] != -1)
 					{
 						throw new eSportsAPIException();
 					}
 
-					results[m][t][w] = Integer.parseInt(OfflineResult.getElementsByTagName("Result").item(0).getTextContent());
+					results[o][m][t][w] = Integer.parseInt(OfflineResult.getElementsByTagName("Result").item(0).getTextContent());
 				}
 			}
 		}
@@ -474,7 +484,7 @@ public class eSportsAPI
 		int[] weathers = gmHelper.getWeatherIDs(true, true);
 		int results[][][] = new int[2][modes.length][tracks.length];
 
-		for(int o = 0; o <= 1; o++)
+		for(int o = 0; o < this.FOS; o++)
 		{
 			for(int m = 0; m < modes.length; m++)
 			{

src/gmHelper.java
@@ -33,7 +33,8 @@ public abstract class gmHelper
 	public final static int WEATHER_ICE  =  4;
 	public final static int WEATHER_RACE =  5;
 
-	// Wird für eSportsAPI.FO_* benötigt...
+	// Wird für eSportsAPI.FO_* benötigt.
+	// Bitte niemals "-1" dafür verwenden!
 	public final static int WEATHER_TICKET = -6;
 
 	// ----------------------- //

commit 87f7b478522308ac6bc4283c8f67fb6b4564d8fc

Fix FFM bug introduced with cd7b5956575586329523dff8651afa788d378e7e

src/HTGT.java
@@ -1460,7 +1460,7 @@ public class HTGT
 
 					if(realUpload && lastUploadedMode > -1 && lastUploadedTrack > -1 && lastUploadedWeather != -1 && !multiGhostEnabled())
 					{
-						if(/*lastFromDefault &&*/ lastUploadedWeather > 0 && newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather] != null)
+						if(/*lastFromDefault &&*/ lastUploadedWeather != gmHelper.WEATHER_TICKET && newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather] != null)
 						{
 							currentGhost = String.format("Der aktuell genutzte Geist ist von %s mit dem Ergebnis %s.%n", newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather].getNickname(), newProfileGhosts[lastUploadedMode][lastUploadedTrack][lastUploadedWeather].getResult());
 						}