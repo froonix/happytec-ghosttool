caa9ea85b8bedde248bde9b442b8bea3dd99445a
Christian Schrötter <cs@fnx.li>
2018-01-02 19:19:46 +0100

    s/2017/2018/

--- src/ProfileException.java
+++ src/ProfileException.java
@@ -1,6 +1,6 @@
 /**
  * ProfileException.java: (Offline)Profiles exception
- * Copyright (C) 2017 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2018 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
