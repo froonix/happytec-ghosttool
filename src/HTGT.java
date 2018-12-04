e9e6ea79272f5a65c94d5066ce6da7562c2a3a8e
Christian Schrötter <cs@fnx.li>
2018-12-04 21:06:40 +0100

    Prepare v0.1.5 release

--- src/HTGT.java
+++ src/HTGT.java
@@ -111,7 +111,7 @@ import javax.swing.table.TableModel;
 public class HTGT
 {
 	// Diverse fixe Konstanten für die Anwendung
-	final private static String    APPLICATION_VERSION = "git-master";
+	final private static String    APPLICATION_VERSION = "0.1.5";
 	final private static String    APPLICATION_NAME    = "HTGT"; // cfg, updates, …
 	final private static String    APPLICATION_TITLE   = "HTGT.app";
 	final private static String    APPLICATION_API     = "HAPPYTEC-eSports-API";
