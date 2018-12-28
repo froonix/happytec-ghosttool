0eb0209a25cab6b76ef289498e79c46cec3ca8ba
Christian Schrötter <cs@fnx.li>
2018-12-29 00:54:11 +0100

    s/2018/2019/

--- src/DynamicMenuItem.java
+++ src/DynamicMenuItem.java
@@ -1,6 +1,6 @@
 /**
  * DynamicMenuItem.java: Avoid anonymous inner classes
- * Copyright (C) 2018 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2019 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/FNX.java
+++ src/FNX.java
@@ -1,6 +1,6 @@
 /**
  * FNX.java: Static helper methods for various stuff
- * Copyright (C) 2018 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2019 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
