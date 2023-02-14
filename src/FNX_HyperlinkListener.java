75f39196bdfdf95e0986818d35a47c5bb5289c0b
Christian Schrötter <cs@fnx.li>
2023-02-14 04:44:27 +0100

    s/2022/2023/

--- src/FNX_HyperlinkListener.java
+++ src/FNX_HyperlinkListener.java
@@ -1,6 +1,6 @@
 /**
  * FNX_HyperlinkListener: Open HTML hyperlinks in browser
- * Copyright (C) 2016-2022 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2016-2023 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
