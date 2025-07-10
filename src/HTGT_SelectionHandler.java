fb54047fbc3b3091b59ecc8efc970dd190eec330
Christian Schrötter <cs@fnx.li>
2025-07-10 14:59:36 +0200

    s/2024/2025/

--- src/HTGT_SelectionHandler.java
+++ src/HTGT_SelectionHandler.java
@@ -1,6 +1,6 @@
 /**
  * HTGT_SelectionHandler.java: Custom table selection
- * Copyright (C) 2016-2024 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2016-2025 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
