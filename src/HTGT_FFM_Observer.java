d8fd80949a20af4c031a8ed49df8be66bd12ddc6
Christian Schrötter <cs@fnx.li>
2025-07-10 14:59:36 +0200

    s/2024/2025/

--- src/HTGT_FFM_Observer.java
+++ src/HTGT_FFM_Observer.java
@@ -1,6 +1,6 @@
 /**
  * HTGT_FFM_Observer.java: Fast-Follow-Mode worker class
- * Copyright (C) 2016-2024 Christian Schrötter <cs@fnx.li>
+ * Copyright (C) 2016-2025 Christian Schrötter <cs@fnx.li>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
